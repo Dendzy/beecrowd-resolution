13228e82f66d5b4935b067c467fa9dbf99705dfc
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-21 20:04:57 -0300

    Create beecrowd_1000.java
    Resolução do exercício 1000 do beecrowd.

---
+++ beecrowd_1000.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class beecrowd_1000 { //Sempre coloque public class Main
+    public static void main(String [] args){
+        //Solução - 
+        System.out.println("Hello World!");
+    }
+}
