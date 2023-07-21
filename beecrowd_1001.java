370a7db08d0907ddcd2a97fb3802a5f9b420491a
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-21 20:04:34 -0300

    Update beecrowd_1001.java

--- beecrowd_1001.java
+++ beecrowd_1001.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.io.IOException;
 
-public class beecrowd_1001 {
+public class beecrowd_1001 { //Sempre coloque public class Main
 
     public static void main (String [] args) {
+        //Solução - 
+
         Scanner leitor = new Scanner(System.in);
         int A = Integer.parseInt(leitor.nextLine());
         int B = Integer.parseInt(leitor.nextLine());
