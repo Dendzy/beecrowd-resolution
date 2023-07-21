ce5b7f60890a2acc7516563b049dbd1471470c87
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-21 19:34:14 -0300

    Create beecrowd_1001.java
    Resolução do exercício 1001 do beecrowd.

---
+++ beecrowd_1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class beecrowd_1001 {
+
+    public static void main (String [] args) {
+        Scanner leitor = new Scanner(System.in);
+        int A = Integer.parseInt(leitor.nextLine());
+        int B = Integer.parseInt(leitor.nextLine());
+        System.out.println("X = " + (A + B));
+
+
+    }
+}
