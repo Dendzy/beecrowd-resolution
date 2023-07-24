8190bc7e17f7297e16dab19ab86c6b40c367aa75
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-24 19:17:11 -0300

    Update beecrowd_1002.java

--- beecrowd_1002.java
+++ beecrowd_1002.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
-import java.io.IOException;
+import java.util.Scanner; //LEMBRE DE IMPORTAR O SCANNER PARA AS ENTRADAS DO USUÁRIO.
 
 public class beecrowd_1002 { //Sempre colocar o "public class Main"
     public static void main(String [] args){
         // Solução -
         Scanner leitor = new Scanner(System.in);
+
         Double raio = Double.parseDouble(leitor.nextLine());
         Double pi = 3.14159;
         Double area = pi * (raio * raio);
         System.out.printf("A=%.4f\n", area);
-
     }
 }
