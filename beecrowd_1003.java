ffbce4e00b3da0d0b0e3d55138f3cafd721ab7bd
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-24 19:17:13 -0300

    Update beecrowd_1003.java

--- beecrowd_1003.java
+++ beecrowd_1003.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+import java.util.Scanner; //LEMBRE DE IMPORTAR O SCANNER PARA AS ENTRADAS DO USUÁRIO.
 
 public class beecrowd_1003 { //Sempre colocar o "public class Main"
     public static void main (String [] args){
         // Solução -
         Scanner leitor = new Scanner(System.in);
+
         int A = Integer.parseInt(leitor.nextLine());
         int B = Integer.parseInt(leitor.nextLine());
         int soma = A + B;
         System.out.println("SOMA = " + soma);
-
     }
 }
