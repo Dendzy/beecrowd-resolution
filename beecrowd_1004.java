6ab97f28c112a7820fd235617531e4335cc0e9b0
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-24 19:17:16 -0300

    Update beecrowd_1004.java

--- beecrowd_1004.java
+++ beecrowd_1004.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
+import java.util.Scanner; //LEMBRE DE IMPORTAR O SCANNER PARA AS ENTRADAS DO USUÁRIO.
 
 public class beecrowd_1004 {
     public static void main (String [] args) { //Sempre colocar o "public class Main"
         // Solução -
         Scanner leitor = new Scanner(System.in);
+    
         int A = Integer.parseInt(leitor.nextLine());
         int B = Integer.parseInt(leitor.nextLine());
         int produto = A * B;
         System.out.println("PROD = " + produto);
-
-
     }
     
 }
