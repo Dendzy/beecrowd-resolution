cc9a85e8a9533939d7360867e7a6f0061ea25181
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-24 17:04:29 -0300

    Create beecrowd_1003.java

---
+++ beecrowd_1003.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class beecrowd_1003 {
+    public static void main (String [] args){
+        Scanner leitor = new Scanner(System.in);
+        int A = Integer.parseInt(leitor.nextLine());
+        int B = Integer.parseInt(leitor.nextLine());
+        int soma = A + B;
+        System.out.println("SOMA = " + soma);
+
+
+    }
+}
