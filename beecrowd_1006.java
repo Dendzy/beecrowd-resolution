e810e81a2bb3faab63ba5725de39ab7a5acf2b99
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-24 18:01:59 -0300

    Create beecrowd_1006.java

---
+++ beecrowd_1006.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Locale; 
+
+public class beecrowd_1006 {
+    public static void main (String [] args) { //Sempre colocar o "public class Main"
+       // Solução -
+        Scanner leitor = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double A = Double.parseDouble(leitor.nextLine());
+        double B = Double.parseDouble(leitor.nextLine());
+        double C = Double.parseDouble(leitor.nextLine());
+        double media = ((A * 2.0) + (B * 3.0) + (C * 5.0)) / 10.0;
+        System.out.printf("MEDIA = %.1f\n", media);
+    }
+}
