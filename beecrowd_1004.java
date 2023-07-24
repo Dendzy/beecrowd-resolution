bbfa848f7ba6a56e7e3cb7d4be23d65fd1a4849f
Dandy <jose.oliveira@dcx.ufpb.br>
2023-07-24 17:17:32 -0300

    Create beecrowd_1004.java

---
+++ beecrowd_1004.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class beecrowd_1004 {
+    public static void main (String [] args) { //Sempre colocar o "public class Main"
+        // Solução -
+        Scanner leitor = new Scanner(System.in);
+        int A = Integer.parseInt(leitor.nextLine());
+        int B = Integer.parseInt(leitor.nextLine());
+        int produto = A * B;
+        System.out.println("PROD = " + produto);
+
+
+    }
+    
+}
