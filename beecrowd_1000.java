29173d6733a9e79d225f0d928a854f923371b6b3
Dandy <jose.oliveira@dcx.ufpb.br>
2023-08-09 14:28:43 -0300

    Removed from main

--- beecrowd_1000.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class beecrowd_1000 { //Sempre coloque public class Main
-    public static void main(String [] args){
-        //Solução - 
-        System.out.println("Hello World!");
-    }
-}
