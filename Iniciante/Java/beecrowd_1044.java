eb37780daa387af9bf8126f7110b93a056a403c6
Dandy <jose.oliveira@dcx.ufpb.br>
2023-08-11 11:04:18 -0300

    Create beecrowd_1044.java

---
+++ Iniciante/Java/beecrowd_1044.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; //LEMBRE DE IMPORTAR O SCANNER PARA AS ENTRADAS DO USUÁRIO.
+
+public class beecrowd_1044 { //Sempre colocar o "public class Main"
+    public static void main (String [] args) {
+        // Solução -
+        Scanner in = new Scanner(System.in);
+
+        String numeros[] = in.nextLine().split(" ");
+
+        int num1 = Integer.parseInt(numeros[0]);
+        int num2 = Integer.parseInt(numeros[1]);
+
+        if (num1 % num2 == 0 || num2 % num1 == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+    }
+}
